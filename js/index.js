561c9de6df6a7454b284154354654b054be179d1
nancyk25 <nancy@zumavc.com>
2017-10-24 19:34:12 -0700

    First commit

---
+++ js/index.js
@@ -0,0 +1,382 @@
+var quotes = [
+ {
+    quote: "That gum you like is coming back in style.",
+    author: "The arm"
+  },
+ {
+    quote: "My Log does not judge.",
+    author: "The Log Lady"
+  },
+ {
+    quote: "Bite the bullet baby",
+    author: "Jacques Renault"
+  },
+ {
+    quote: "Agent Cooper loves coffee.",
+    author: "Audrey Horne"
+  },
+ {
+    quote: "I plan on writing an epic poem about this pie.",
+    author: "Gordon Cole"
+  },
+ {
+    quote: "It's like I’m having the most beautiful dream and the most terrible nightmare at once.",
+    author: "Donna Hayward"
+  },
+ {
+    quote: "It’s happening again.",
+    author: "The Giant"
+  },
+  {
+quote:"Hiding from your fear doesn’t make your fear go away.",
+author:"Annie Hayward"
+},
+{
+quote: "Every day, once a day, give yourself a present. Don’t plan it, don’t wait for it, just let it happen. It could be a new shirt at the men's store, a catnap in your office chair or two cups of good hot black coffee.",
+author:"Dale Cooper"
+},
+{
+quote:"And Styrofoam never dies for as long as you live.",
+author:"Andy Brennan"
+},
+{
+quote: "I just need some me time",
+author: "Lucy Brennan"
+},
+{
+quote:"Cotton balls!",
+author:"Nadine Hurley"
+},
+{
+quote:"I just need some me time",
+author:"Lucy Brennan"
+},
+{
+quote:"The owls are not what they seem.",
+author:"The Log Lady"
+},
+{
+quote:"There was a fish in the percolator!",
+author:"Pete Martell"
+},
+{
+quote:"The problems of our society are of sexual nature",
+author:"Dr. Jacoby"
+},
+{
+quote:"I feel like I know her but sometimes my arms bend back.",
+author:"Laura Palmer"
+},
+{
+quote:"Uh, what is shenanigans?",
+author:"Josie Packard"
+},
+  {
+quote:"To be perfectly honest, I think I’m all little over my head.",
+author:"Audrey Horne"
+},
+{
+quote: "Fix your hearts or die.",
+author:"Gordon Cole"
+},
+{
+quote:"You know, this is – excuse me – a damn fine cup of coffee.",
+author:"Dale Cooper"
+},
+{
+quote:"God I love this music.",
+author:"Audrey Horne"
+},
+{
+quote:"I consumed 15 donuts today, Diane. All jelly.",
+author:"Dale Cooper"
+},
+{
+quote:"Nothing beats the taste sensation when maple syrup collides with ham.",
+author:"Dale Cooper"
+},
+{
+quote:"Diane, I’m holding in my hand a small box of chocolate bunnies.",
+author:"Dale Cooper"
+},
+{
+quote:"HelloooOOOooo!",
+author:"Dale Cooper"
+},
+{
+quote:"Jade give two rides.",
+author:"Dale Cooper"
+},
+{
+quote:"Albert, I hate to admit this, but I don't understand this situation at all.",
+author:"Gordon Cole"
+},
+{
+quote:"Fuck Gene Kelly, motherfucker!",
+author:"Albert Rosenfield"
+},
+{
+quote:"I’ll see you again in twenty-five years.",
+author:"Laura Palmer"
+},
+{
+quote:"I am not your foot.",
+author:"Jerry Horne’s foot"
+},
+{
+quote:"Got a light?",
+author:"Woodsman"
+},
+{
+quote:"How's Annie? How's Annie? How's Annie?",
+author:"Bob [possessing Dale Cooper]"
+},
+{
+quote:"DON'T LET'EM RATTLE YOU, COOP. THESE GUYS MAKE A LIVING LOOKING THROUGH OTHER PEOPLE'S DRAWERS. MAY A SMILE BE YOUR UMBRELLA. WE'VE ALL HAD OUR SOCKS TOSSED AROUND. CATCH YOU LATER.",
+author:"Gordon Cole"
+},
+{
+quote:"New shoes",
+author:"Leo Johnson"
+},
+  {
+ quote: "I have no idea where this will lead us, but I have a definite feeling it will be a place both wonderful and strange",
+ author: "Dale Cooper"
+  },
+
+  {
+   quote: "The owls are not what they seem",
+    author: "Giant"
+  },
+  {
+    quote: "Through the darkness of future past The magician longs to see One chants out between two worlds Fire   walk with me.", 
+   author: "Leland Palmer"
+  },
+  {
+    quote: "You may think I've gone insane, but I promise I will kill again!",
+    author: "Leland Palmer"
+  },
+  {
+    quote: "When you see me again, it won't be me.",
+    author: "Man from another place"
+  },
+  {
+  quote: "Where we're from, the birds sing a pretty song... and there's always music in the air.",
+  author: "Man from another place"
+  },
+  {
+  quote: "...That's what you do in a town where a yellow light still means slow down, not go faster.",
+    author: "Dale Cooper"
+  },
+  {
+    quote: "I feel like I know her... but sometimes my arms bend back.",
+    author: "Laura Palmer"
+  },
+  {
+    quote: "I am 100% sure that we're not completely sure.",
+    author: "Jerry Horne"
+  },
+  {
+    quote: "Achievement is its own reward, pride obscures it.",
+    author: "Major Briggs"
+  },
+  {
+    quote: "One day my log will have something to say about this.",
+    author: "The Log Lady"
+  },
+  {
+    quote: "I believe I was visited by a giant last night. Twice",
+    author: "Dale Cooper"
+  },
+  {
+    quote: "Any relations with the dwarf?",
+    author: "Agent Albert Rosenfield"
+  },
+  {
+    quote: "My log saw something that night",
+    author: "The Log Lady"
+  },
+  {
+    quote: "Cooper, you remind me today of a small Mexican chi-wow-wow.",
+    author: "Bureau Chief Gordon Cole"
+  },
+  {
+    quote: "The bad news is that your father has bought a condo in Flip City, and the good news is that he is about to win the Civil War.",
+    author: "Bobby Briggs"
+  },
+  {
+    quote: "The only thing Columbus discovered was that he was lost!",
+    author: "Windom Earle"
+  },
+  {
+    quote: "Fellas, coincidence and fate figure largely in our lives",
+    author: "Dale Cooper"
+  },
+  {
+    quote: "Harry, is that bag smiling?",
+    author: "Dale Cooper"
+  },
+  {
+    quote: "It's not the first time, it won't be the last, but I'm in that doghouse again.",
+    author: "Big Ed Hurley"
+  },
+  {
+    quote: "When you see me again it won't be me.",
+    author: "The arm"
+  },
+    {
+    quote: "I've got a lot of cutting and pasting to do, gentlemen, so please, why don't you return to your porch rockers and resume whittling?",
+    author: "Albert Rosenfield"
+    },
+  {
+    quote:"My vision ended and I awoke with a tremendous feeling of optimism and confidence in you and your future. That was my vision of you. I’m so glad to have had this opportunity to share it with you.",
+    author:"Colonel Briggs"
+  },
+  {
+    quote:"[Windom Earle: “What do you fear most in he world?”] “The possibility that love is not enough.”",
+    author:"Colonel Briggs"
+  },
+  {
+    quote:"Achievement is it’s own reward. Pride obscures it.",
+    author:"Colonel Briggs"
+  },
+  {
+    quote:"I’m a waitress in a diner. I’ve never been compared to a goddess before.",
+    author:"Shelly Johnson"
+  },
+  {
+    quote:"Swimming in my mind at this time, literally, is my new hydroponic Indica Sativa hybrid, a touch of the mythic AK-47 by way of the Amsterdam express. It's baked into this banana bread and infused in this potent, spreadable jam that's ideal for creative sojourns of a solitary nature. Wheels up! ",
+    author:"Jeremy Horne"
+  },
+  {
+    quote:"If I had a nickel for every cigarette your mom smoked, I'd be dead. ",
+    author:"Donna Hayward"
+  },
+  {
+    quote:"He has the cutest buns.",
+    author:"Nadine Hurley"
+  },
+{
+    quote:"I’m going to have the world’s first one hundred percent quiet runner.",
+    author:"Nadine Hurley"
+  },
+  {
+    quote:"We have to dance – for Laura!",
+    author:"Leland Palmer"
+  },
+  {
+    quote:"Sometimes the urge to do bad is nearly overpowering.",
+    author:"Benjamin Horne"
+  },
+   {
+    quote:"It’s not really a place it’s a feeling. Sometimes riding at night, I punch off the headlights and roll the throttle in just rocket blind into the dark.",
+    author:"James Hurley"
+  },
+     {
+    quote:"She won’t speak to me and I don’t know why. I don’t know why she is doing what she is doing.",
+    author:"Andy Brennan"
+  },
+     {
+    quote:"Would you care to leave a message?",
+    author:"Lucy Moran"
+  },
+     {
+    quote:"I have no idea where this will lead us, but I have a definite feeling it will be a place both wonderful and strange.",
+    author:"Dale Cooper"
+  },
+  {
+    quote:"There they are Albert faces of stone",
+    author:"Gordon Cole"
+  },
+  {
+    quote:"Is it about the bunnies?",
+    author:"Lucy Moran"
+  },
+  {
+    quote:"Is it future ? Or is it past?",
+    author:"Mike"
+  },
+  {
+    quote:"Shovel your want out of your shit and into the truth!",
+    author:"Dr. Lawrence Jacoby"
+  },
+  {
+    quote:"I'll call you at the bar.",
+    author:"Gordon Cole"
+  },
+  {
+    quote:"Tres Chic!",
+    author:"Gordon Cole"
+  },
+  {
+    quote: "There's no backup for this",
+    author: "Diane"
+    
+  },
+  {
+    quote: "Thank you again Mr. Jackpots!",
+    author: "Casino Lady"
+  },
+  {
+    quote: "Fuck you Albert!",
+    author: "Diane"
+    
+  },
+  {
+    quote:"Dirty bearded men!",
+    author:"Gordon Cole"
+  },
+  {
+    quote:"He's dead",
+    author:"Gordon Cole"
+  },
+  {
+    quote:"Even our fucking orphanage had a gym set",
+    author:"Bradley Mitchum"
+  }
+];
+
+var currentQuote
+var newQuote
+
+function randomizeQuote(){
+  do{
+  var index = Math.floor((Math.random()*quotes.length))
+ } while (currentQuote === index);
+  newQuote = quotes[index];
+  currentQuote = index;
+  $("#quote-text").html('"' + quotes[index].quote + '"');
+  $("#quote-author").html('-' + quotes[index].author);
+};
+
+function tweet () {
+    var tweetQuote = quotes[currentQuote].quote;
+    
+    if (tweetQuote.length > 100) {
+      tweetQuote = tweetQuote.substr(0,100).match(/(^.+)\s/)[1] + "...";
+    
+    var tweetAuthor = quotes[currentQuote].author;
+      tweetQuote = encodeURI(tweetQuote);
+      tweetAuthor = encodeURI(tweetAuthor);
+      window.open("https://twitter.com/intent/tweet?text=" + tweetQuote  + " - " + tweetAuthor);
+    };
+}
+
+
+$(document).ready(function(){
+
+  $("#new-quote").click(function(){
+   randomizeQuote();
+  });
+  
+  $("#tweet-quote").click(function(){
+    tweet();
+  });
+  
+  $("#tumblr-quote").click(function(){
+    $('#tumblr-quote').attr('href', 'https://www.tumblr.com/widgets/share/tool?posttype=quote&tags=quotes,freecodecamp&caption='+encodeURI(quotes[currentQuote].quote)+'&content=' + encodeURI(quotes[currentQuote].author)+'&canonicalUrl=https%3A%2F%2Fwww.tumblr.com%2Fbuttons&shareSource=tumblr_share_button');
+
+  });
+  
+  
+});
